34a862f078966ac156188021b073d6e4a4390b22
Daisy Good <daisy.m.good@gmail.com>
2016-10-25 12:28:51 -0500

    profileEdit V1.2

--- src/components/profileEdit.jsx
+++ src/components/profileEdit.jsx
@@ -5,41 +5,64 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import TextField from 'material-ui/TextField';
 import MyTheme from '../theme/theme.js';
+import { findUser, updateUser } from '../services/userServices.js';
 
-const ProfileEdit = () => (
-  <MuiThemeProvider muiTheme={getMuiTheme(MyTheme)}>
-    <div className="middle">
-    <TextField
-      hintText="First Name"
-      floatingLabelText="First Name"
-    /><br />
-    <TextField
-      hintText="Last Name"
-      floatingLabelText="Last Name"
-    /><br />
-    <TextField
-      hintText="Location"
-      floatingLabelText="Location"
-    /><br />
-    <TextField
-      hintText="Dog Name"
-      floatingLabelText="Dog Name"
-    /><br />
-    <TextField
-      hintText="Dog Breed"
-      floatingLabelText="Dog Breed"
-    /><br />
-    <TextField
-      hintText="Dog Age"
-      floatingLabelText="Dog Age"
-    /><br />
-    <TextField
-      hintText="Dog Profile Pic"
-      floatingLabelText="Dog Profile Pic"
-    /><br />
-  </div>
-  </MuiThemeProvider>
-);
+class ProfileEdit extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      currentUser: {}
+    }
+  }
+
+  componentDidMount() {
+    var self = this;
+    findUser()
+      .then((user) => {
+        self.setState({'currentUser': user});
+        console.log(this.state.currentUser.firstname)
+    })
+  }
+
+  render () {
+    return (
+        <MuiThemeProvider muiTheme={getMuiTheme(MyTheme)}>
+          <div className="middle">
+          <TextField
+            hintText="First Name"
+            floatingLabelText="First Name"
+            defaultValue = {this.state.currentUser.firstname}
+          /><br />
+          <TextField
+            hintText="Last Name"
+            floatingLabelText="Last Name"
+          /><br />
+          <TextField
+            hintText="Location"
+            floatingLabelText="Location"
+          /><br />
+          <TextField
+            hintText="Dog Name"
+            floatingLabelText="Dog Name"
+          /><br />
+          <TextField
+            hintText="Dog Breed"
+            floatingLabelText="Dog Breed"
+          /><br />
+          <TextField
+            hintText="Dog Age"
+            floatingLabelText="Dog Age"
+          /><br />
+          <TextField
+            hintText="Dog Profile Pic"
+            floatingLabelText="Dog Profile Pic"
+          /><br />
+        </div>
+      </MuiThemeProvider>
+    )
+  }
+
+}
 
 
 module.exports = ProfileEdit;
