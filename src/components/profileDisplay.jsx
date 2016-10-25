ae9e737fb5615479d57d9390e9ceac8912b51611
Ivey Topaz <iveytopaz@gmail.com>
2016-10-25 14:39:51 -0400

    adding loading div to pages

--- src/components/profileDisplay.jsx
+++ src/components/profileDisplay.jsx
@@ -5,6 +5,7 @@ var React = require('react');
 //var UsersPage = require('./usersPage.jsx');
 var Profile = require('./profile.jsx')
 import { hashHistory } from 'react-router';
+import Loading from './loading.jsx'
 
 class ProfileDisplay extends React.Component {
   constructor(props) {
@@ -31,7 +32,7 @@ class ProfileDisplay extends React.Component {
       )
     } else {
       return (
-        <div>Loading</div>
+        <Loading />
       )
     }
   }
