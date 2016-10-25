9a1aac5b60048e7919a7f44c724ccbfba8dc664b
katgurdak <katgurdak@gmail.com>
2016-10-25 15:59:02 -0700

    Made changes to userServices
    - Added getLatLng function (geocodes location to coordinates)
    - Added updateDb function (ajax PUT request)
    - Changed updateUser function to use functions mentioned above

--- src/services/userServices.js
+++ src/services/userServices.js
@@ -4,6 +4,29 @@ import Promise from 'bluebird'
 
 module.exports =  {
 
+  getLatLng: function(address) {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: 'api/geocode?loc=' + address,
+        type: 'GET',
+        success: resolve,
+        error: reject
+      });
+    });
+  },
+
+  updateDb: function(newProps) {
+    return new Promise((resolve, reject) => {
+      $.ajax({
+        url: 'api/users',
+        type: 'PUT',
+        data: newProps,
+        success: resolve,
+        error: reject
+      });
+    });
+  },
+
   getUsers: function() {
     return new Promise((resolve, reject) => {
       $.ajax({
@@ -42,15 +65,17 @@ module.exports =  {
 
   updateUser: function(newProps) {
     newProps.dbId = localStorage.getItem('mongoUserId');
-    return new Promise((resolve, reject) => {
-      $.ajax({
-        url: 'api/users',
-        type: 'PUT',
-        data: newProps,
-        success: resolve,
-        error: reject
-      });
-    });
+    if (newProps.loc) {
+      let self = this;
+      this.getLatLng(newProps.loc)
+        .then(function(results) {
+          newProps.lat = results.lat;
+          newProps.lng = results.lng;
+          self.updateDb(newProps);
+        });
+    } else {
+      this.updateDb(newProps);
+    }
   },
 
   deleteUser: function(dbId) {
