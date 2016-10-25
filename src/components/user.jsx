91a40380a72787e2e874fb4fdae4d57b5fa972b2
Ivey Topaz <iveytopaz@gmail.com>
2016-10-25 15:56:36 -0400

    making meetupDialog component

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -2,58 +2,9 @@
 
 import React from 'react';
 import UserList from './userList.jsx';
-import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import getMuiTheme from 'material-ui/styles/getMuiTheme';
-import RaisedButton from 'material-ui/RaisedButton';
-import FlatButton from 'material-ui/FlatButton';
-import MyTheme from '../theme/theme.js';
-import Dialog from 'material-ui/Dialog';
 import MeetupCreation from './meetupCreation.jsx'
+import MeetupDialog from './meetupDialog.jsx'
 
-class MeetupDialog extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      open: false,
-    };
-    this.handleOpen = this.handleOpen.bind(this)
-    this.handleClose = this.handleClose.bind(this)
-  }
-
-  handleOpen() {
-    this.setState({open: true});
-  };
-
-  handleClose() {
-    this.setState({open: false});
-  };
-
-  render() {
-    const actions = [
-      <FlatButton
-        label="Cancel"
-        primary={true}
-        onTouchTap={this.handleClose}
-      />
-      <FlatButton
-        label="Create Event"
-        primary={true}
-        keyboardFocused={true}
-        onTouchTap={this.handleClose}
-      />
-    ]
-    return (
-      <MuiThemeProvider muiTheme={getMuiTheme(MyTheme)}>
-        <div>
-          <RaisedButton onTouchTap={this.handleOpen} label="Let's Meetup!" secondary={true}/>
-          <Dialog title="Meetup Creation" actions={actions} modal={false} open={this.state.open} onRequestClose={this.handleClose}>
-            <MeetupCreation />
-          </Dialog>
-        </div>
-      </MuiThemeProvider>
-    )
-  }
-}
 
 class UserDisplay extends React.Component {
 
