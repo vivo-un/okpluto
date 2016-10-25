0607704e1412e8e19a4cb49d8fa026d84dced3e2
Daisy Good <daisy.m.good@gmail.com>
2016-10-25 11:05:43 -0500

    Created color theme

--- src/components/profileEdit.jsx
+++ src/components/profileEdit.jsx
@@ -4,9 +4,10 @@ import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import TextField from 'material-ui/TextField';
+import MyTheme from '../theme/theme.js';
 
 const ProfileEdit = () => (
-  <MuiThemeProvider muiTheme={getMuiTheme()}>
+  <MuiThemeProvider muiTheme={getMuiTheme(MyTheme)}>
     <div>
     <TextField
       hintText="Hint Text"
