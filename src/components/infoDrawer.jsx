e31d9ab3428861d2d14f85060de4fba526b46135
Ivey Topaz <iveytopaz@gmail.com>
2016-10-27 14:07:33 -0400

    adding info drawer out of sheer optimism :)

---
+++ src/components/infoDrawer.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MyTheme from '../theme/theme.js';
+import Drawer from 'material-ui/Drawer'
+import ProfileDisplay from './profileDisplay.jsx';
+import { findUser } from '../services/userServices.js'
+
+
+class InfoDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+
+  render() {
+    if (this.props.user._id) {
+    return (
+      <MuiThemeProvider muiTheme={getMuiTheme(MyTheme)}>
+        <Drawer open={this.props.open}>
+          <h4> Hide </h4>
+          <h3> Your Info </h3>
+          <ProfileDisplay user={this.props.user} />
+        </Drawer>
+      </MuiThemeProvider>
+    )
+    } else{
+      return (<div></div>)
+    }
+  }
+}
+
+module.exports = InfoDrawer
