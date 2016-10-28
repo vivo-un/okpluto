b944ec06cbc8286e2770dee9b9c3df201407461c
Ivey Topaz <iveytopaz@gmail.com>
2016-10-27 22:07:08 -0400

    adding appbar to logged in navigation

--- src/components/nav-loggedIn.jsx
+++ src/components/nav-loggedIn.jsx
@@ -1,12 +1,41 @@
 "use strict";
 
-var React = require('react');
+import * as Colors from 'material-ui/styles/colors';
+import React from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MyTheme from '../theme/theme.js';
+import AppBar from 'material-ui/AppBar'
+import FlatButton from 'material-ui/FlatButton';
 import { hashHistory } from 'react-router';
+
 class NavLoggedIn extends React.Component {
 
   render () {
+    const barStyle= {
+      position: 'fixed',
+      backgroundColor: Colors.grey100
+    }
+    const titleStyle = {
+      textAlign: 'left'
+    }
     return (
-      <div>
+      <MuiThemeProvider muiTheme={getMuiTheme(MyTheme)}>
+        <AppBar
+          style={barStyle}
+          title={<span style={titleStyle}>Ok Pluto</span>}
+          showMenuIconButton={false}
+          iconElementRight={<FlatButton label="Login / Signup" />}
+          onRightIconButtonTouchTap={this.props.auth.login.bind(this)}
+        />
+      </MuiThemeProvider>
+    )
+  }
+}
+
+module.exports = NavLoggedIn;
+
+/* <div>
         <nav className="navbar navbar-default navbar-fixed-top">
           <div className="container">
             <div className="navbar-header">
@@ -29,9 +58,4 @@ class NavLoggedIn extends React.Component {
             </div>
           </div>
         </nav>
-      </div>
-    )
-  }
-}
-
-module.exports = NavLoggedIn;
+      </div> */
