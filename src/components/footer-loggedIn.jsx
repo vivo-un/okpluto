11f8b40dde225026d93f130416fa609c4beed2d8
katgurdak <katgurdak@gmail.com>
2016-10-29 15:04:18 -0700

    Created footer-loggedIn component

---
+++ src/components/footer-loggedIn.jsx
@@ -0,0 +1,20 @@
+"use strict";
+
+import React from 'react';
+import { hashHistory } from 'react-router';
+
+const FooterLoggedIn = (props) => (
+  <footer className="primary-footer">
+    <small>&copy; Ok Pluto</small>
+    <nav className="nav">
+      <ul>
+        <li><a onClick={() => hashHistory.push('/users')}>Users</a></li>
+        <li><a onClick={() => hashHistory.push('/events')}>Events</a></li>
+        <li><a onClick={() => hashHistory.push('/profile')}>Profile</a></li>
+        <li><a href="#">Back to top</a></li>
+      </ul>
+    </nav>
+  </footer>
+)
+
+module.exports = FooterLoggedIn;
