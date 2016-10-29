421ab87f08fbe5da86fffda354eba4edfa0b7604
Daisy Good <daisy.m.good@gmail.com>
2016-10-29 11:31:24 -0500

    Profile pic is not filling the div now

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -8,6 +8,8 @@ import DialogButton from './dialogButton.jsx'
 import {Card, CardActions, CardHeader, CardMedia, CardTitle, CardText} from 'material-ui/Card';
 import FlatButton from 'material-ui/FlatButton';
 
+
+
 class UserDisplay extends React.Component {
 
   constructor(props) {
@@ -23,28 +25,28 @@ class UserDisplay extends React.Component {
     }
     return (
       <div>
-      <Card>
-        <CardHeader
-          title={this.user.dogname}
-          subtitle={'At: ' + this.user.loc}
-          avatar={this.user.profilepic}
-          actAsExpander={false}
-          showExpandableButton={false}
-        />
-        <CardMedia>
-          <img src={this.user.picLink || this.user.profilepic} alt="Pic" />
-        </CardMedia>
-        <CardText expandable={false}>
-
-        </CardText>
-        <CardActions>
-          <DialogButton userId={this.user._id} lat={this.user.lat} lng={this.user.lng} type={this.props.type} userInfo={this.props.userInfo} resetUserInfo={this.props.resetUserInfo} toggleDrawer={this.props.toggleDrawer} toggleProfile={this.props.toggleProfile}/>
-        </CardActions>
-      </Card>
-
-
-
+        <Card>
+          <CardMedia >
+            <div className="card-img">
+            <img src={this.user.picLink || this.user.profilepic} className="img" alt="Pic"/>
             </div>
+          </CardMedia>
+          <CardHeader
+            title={this.user.dogname}
+            subtitle={'At: ' + this.user.loc}
+            avatar={this.user.profilepic}
+            actAsExpander={false}
+            showExpandableButton={false}
+          />
+
+          <CardText expandable={false}>
+
+          </CardText>
+          <CardActions>
+            <DialogButton userId={this.user._id} lat={this.user.lat} lng={this.user.lng} type={this.props.type} userInfo={this.props.userInfo} resetUserInfo={this.props.resetUserInfo} toggleDrawer={this.props.toggleDrawer} toggleProfile={this.props.toggleProfile}/>
+          </CardActions>
+        </Card>
+      </div>
     )
   }
 
