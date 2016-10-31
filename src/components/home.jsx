d92c2891b562dbd35896cb8b683f6aa41798e273
jkennedy3161 <kennedyjt88@gmail.com>
2016-10-31 12:03:17 -0700

    'more cleaning up, removing double quotes for single quotes'

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,4 +1,4 @@
-"use strict";
+'use strict';
 import React, { PropTypes as T } from 'react';
 import AuthService from '../utils/AuthService.jsx';
 import Navigation from './nav.jsx';
@@ -20,9 +20,9 @@ class IconItem extends React.Component {
   };
   render() {
     return (
-      <div className="col-md-4">
+      <div className='col-md-4'>
         <a href={this.props.link} onClick={this.props.onTouchTap}>
-          <img src={this.props.url} className="img-circle"/>
+          <img src={this.props.url} className='img-circle'/>
         </a>
         <h2>{this.props.display}</h2>
         <p>{this.props.info}</p>
@@ -37,9 +37,9 @@ class TeamIcon extends React.Component {
   };
   render() {
     return (
-      <div className="col-md-3" >
+      <div className='col-md-3' >
         <a href={this.props.link}>
-          <img src={this.props.url} className="img-circle"/>
+          <img src={this.props.url} className='img-circle'/>
         </a>
         <h3>{this.props.display}</h3>
       </div>
@@ -53,66 +53,66 @@ class Home extends React.Component {
     return (
       <div>
         <Navigation auth={this.props.auth}/>
-        <div className="jumbotron main-image" >
-          <div className="container">
+        <div className='jumbotron main-image' >
+          <div className='container'>
             <h1>Ok Pluto</h1>
             <h2>Online playground</h2>
             <h2>for your best friend</h2>
           </div>
         </div>
 
-        <div className="row section-heading">
+        <div className='row section-heading'>
           <p>Get to know fellow dog owners in your area</p>
         </div>
 
-        <div className="container icons">
-          <div className="row">
+        <div className='container icons'>
+          <div className='row'>
             <IconItem
               url={Connect}
-              display={"CONNECT"}
-              info={"OkPluto instantly puts you in touch with other dog lovers from around the world"}
+              display={'CONNECT'}
+              info={'OkPluto instantly puts you in touch with other dog lovers from around the world'}
               onTouchTap={this.props.auth.signup.bind(this)}
             />
             <IconItem
               url={MeetUp}
-              display={"MEET UP"}
-              info={"We make it easy for you to set up walks and dog park meet ups with other local dog owners"}
+              display={'MEET UP'}
+              info={'We make it easy for you to set up walks and dog park meet ups with other local dog owners'}
               onTouchTap={this.props.auth.signup.bind(this)}
             />
             <IconItem
               url={House}
-              display={"ADOPT A DOG"}
-              info={"Don't have a dog? No worries! Adopt your new best friend at a shelter near you!"}
-              link={"https://www.petfinder.com/animal-shelters-and-rescues/search"}
+              display={'ADOPT A DOG'}
+              info={'Don\'t have a dog? No worries! Adopt your new best friend at a shelter near you!'}
+              link={'https://www.petfinder.com/animal-shelters-and-rescues/search'}
             />
           </div>
         </div>
 
-        <div className="row section-heading alt">
+        <div className='row section-heading alt'>
           <p>Our Development Team</p>
         </div>
 
-        <div className="team">
-          <div className="row">
+        <div className='team'>
+          <div className='row'>
             <TeamIcon
               url={Daisy}
-              display={"Daisy Good"}
-              link={"https://www.linkedin.com/in/daisy-good-49a2a46a"}
+              display={'Daisy Good'}
+              link={'https://www.linkedin.com/in/daisy-good-49a2a46a'}
             />
             <TeamIcon
               url={Kat}
-              display={"Kat Gurdak"}
-              link={"https://www.linkedin.com/in/katgurdak"}
+              display={'Kat Gurdak'}
+              link={'https://www.linkedin.com/in/katgurdak'}
             />
             <TeamIcon
               url={Jarrett}
-              display={"Jarrett Kennedy"}
-              link={"https://www.linkedin.com/in/jarrettk"}
+              display={'Jarrett Kennedy'}
+              link={'https://www.linkedin.com/in/jarrettk'}
             />
             <TeamIcon
               url={Ivey}
-              display={"Ivey Topaz"}
-              link={"https://www.linkedin.com/in/ivey-topaz-765a85124"}
+              display={'Ivey Topaz'}
+              link={'https://www.linkedin.com/in/ivey-topaz-765a85124'}
             />
           </div>
         </div>
