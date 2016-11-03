7206e429dc4672fbf2e81eefc5e98929e0a1c7f4
Vi Vo <vi.uyen.vo@gmail.com>
2016-11-03 12:13:23 -0700

    moved inside cb

--- src/app.jsx
+++ src/app.jsx
@@ -29,43 +29,44 @@ axios.get('/env')
     console.log(response.data);
     env = response.data;
     auth = new AuthService(env, 'vivou.auth0.com');
+    // Setting up auth service
+
+
+    // check for authenication in all protected routes
+    const requireAuth = (nextState, replace) => {
+      if (!auth.loggedIn()) {
+        replace ({ pathname: '/'})
+      }
+    }
+
+    // Allows onTouchTap to work in material ui components
+    injectTapEventPlugin();
+
+    // Container is a parent route that passes authenication service to all
+    // of its children
+    // Info drawer is a parent route that loads in the current users info,
+    // and passes it as props to all direct child routes
+    // "access_token" route is so auth0 login has a route that matches it
+    // otherwise login will fail
+    ReactDOM.render(
+      <Router history={hashHistory}>
+        <Route path="/" component={Container} auth={auth}>
+          <IndexRoute component={Home} />
+          <Route component={InfoDrawer} >
+            <Route path="/users" component={UsersPage} onEnter={requireAuth} />
+            <Route path="/events" component={Events} onEnter={requireAuth} creation={false}/>
+            <Route path="/profile" component={Profile} onEnter={requireAuth} creation={false}/>
+          </Route>
+          <Route path="/creation" component={ProfileCreation} onEnter={requireAuth} creation={true}/>
+          <Route path="access_token=:token" component={Loading} />
+        </Route>
+      </Router>, $('#app')[0]
+    );
+
   })
   .catch(function(err){
     console.log(error);
   });
 
-// Setting up auth service
-
-
-// check for authenication in all protected routes
-const requireAuth = (nextState, replace) => {
-  if (!auth.loggedIn()) {
-    replace ({ pathname: '/'})
-  }
-}
-
-// Allows onTouchTap to work in material ui components
-injectTapEventPlugin();
-
-// Container is a parent route that passes authenication service to all
-// of its children
-// Info drawer is a parent route that loads in the current users info,
-// and passes it as props to all direct child routes
-// "access_token" route is so auth0 login has a route that matches it
-// otherwise login will fail
-ReactDOM.render(
-  <Router history={hashHistory}>
-    <Route path="/" component={Container} auth={auth}>
-      <IndexRoute component={Home} />
-      <Route component={InfoDrawer} >
-        <Route path="/users" component={UsersPage} onEnter={requireAuth} />
-        <Route path="/events" component={Events} onEnter={requireAuth} creation={false}/>
-        <Route path="/profile" component={Profile} onEnter={requireAuth} creation={false}/>
-      </Route>
-      <Route path="/creation" component={ProfileCreation} onEnter={requireAuth} creation={true}/>
-      <Route path="access_token=:token" component={Loading} />
-    </Route>
-  </Router>, $('#app')[0]
-);
 
 
