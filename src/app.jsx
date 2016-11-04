8d8ed6cc4adee581c86f85aaed2e9360ae5f6a91
Vi Vo <vi.uyen.vo@gmail.com>
2016-11-03 21:04:07 -0700

    took back routes

--- src/app.jsx
+++ src/app.jsx
@@ -50,20 +50,22 @@ axios.get('/env')
     // and passes it as props to all direct child routes
     // "access_token" route is so auth0 login has a route that matches it
     // otherwise login will fail
-    ReactDOM.render(
-      <Router history={hashHistory}>
-        <Route path="/" component={Container} auth={auth}>
-          <IndexRoute component={Home} />
-          <Route component={InfoDrawer} >
-            <Route path="/users" component={UsersPage} onEnter={requireAuth} />
-            <Route path="/events" component={Events} onEnter={requireAuth} creation={false}/>
-            <Route path="/profile" component={Profile} onEnter={requireAuth} creation={false}/>
-          </Route>
-          <Route path="/creation" component={ProfileCreation} onEnter={requireAuth} creation={true}/>
-          <Route path="access_token=:token" component={Loading} />
-        </Route>
-      </Router>, $('#app')[0]
-    );
+ReactDOM.render(
+  <Router history={hashHistory}>
+    <Route path="/" component={Container} auth={auth}>
+      <IndexRoute component={Home} />
+      <Route component={InfoDrawer} >
+        <Route path="/users" component={UsersPage} onEnter={requireAuth} />
+        <Route path="/events" component={Events} onEnter={requireAuth} creation={false}/>
+        <Route path="/profile" component={Profile} onEnter={requireAuth} creation={false}/>
+        <Route path="/dogprofile" component={DogProfile} onEnter={requireAuth} creation={false}/>
+        <Route path="/rental" component={DogRental} onEnter={requireAuth} creation={false}/>
+      </Route>
+      <Route path="/creation" component={ProfileCreation} onEnter={requireAuth} creation={true}/>
+      <Route path="access_token=:token" component={Loading} />
+    </Route>
+  </Router>, $('#app')[0]
+);
 
   })
   .catch(function(err){
